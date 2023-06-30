04995e194872c38287b7b4b250b6033488c51b10
Hesham Gasser <heshamgasser32@gmail.com>
2023-06-30 16:37:29 +0300

    bottomNavigationBar Color

--- lib/provider/home_provider.dart
+++ lib/provider/home_provider.dart
@@ -3,28 +3,32 @@ import 'package:movies_app/screens/browse_screen/browse_screen.dart';
 import 'package:movies_app/screens/home_screen/home_screen.dart';
 import 'package:movies_app/screens/search_screen.dart';
 import 'package:movies_app/screens/watchList_screen.dart';
+import 'package:movies_app/styles/app_color.dart';
 
 class HomeProvider extends ChangeNotifier {
   List <Widget> screensWidget = [HomeScreen(), SearchScreen(), BrowseScreen(), WatchList()];
 
   List <BottomNavigationBarItem> bottomItems = [
     BottomNavigationBarItem(
-      backgroundColor: Colors.black,
+      backgroundColor: backgroundColor,
         icon: Icon(
           Icons.home,
         ),
         label: 'HOME'),
     BottomNavigationBarItem(
+        backgroundColor: backgroundColor,
         icon: Icon(
           Icons.search,
         ),
         label: 'SEARCH'),
     BottomNavigationBarItem(
+        backgroundColor: backgroundColor,
         icon: Icon(
           Icons.movie_creation,
         ),
         label: 'BROWSE'),
     BottomNavigationBarItem(
+        backgroundColor: backgroundColor,
         icon: Icon(
           Icons.bookmarks,
         ),
