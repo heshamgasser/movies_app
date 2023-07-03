b598bf6e26acf942ac1e26b4b0ff0630068094b3
Hesham Gasser <heshamgasser32@gmail.com>
2023-07-03 23:22:38 +0300

    handle null text in Movie Details Widget

--- lib/screens/browse_screen/browse_screen.dart
+++ lib/screens/browse_screen/browse_screen.dart
@@ -1,9 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movies_app/remote/api_manager.dart';
-import 'package:movies_app/screens/browse_screen/browse_screen_bloc/browse_screen_cubit.dart';
-import 'package:movies_app/screens/browse_screen/browse_screen_bloc/browse_screen_state.dart';
 import 'package:movies_app/screens/browse_screen/movies_by_category.dart';
 import 'package:movies_app/styles/app_color.dart';
 
@@ -86,68 +83,7 @@ class BrowseScreen extends StatelessWidget {
 
 
 
-    //   BlocProvider(create: (context) => BrowseScreenCubit()..getMoviesCategory(),
-    // child: BlocConsumer<BrowseScreenCubit, BrowseScreenStates>(
-    //   listener: (context, state) {
-    //
-    //   },
-    //   builder: (context, state) {
-    //     return Padding(
-    //       padding: EdgeInsets.only(left: 25.w, right: 25.w, bottom: 18.h, top: 40.h),
-    //       child: Column(
-    //         crossAxisAlignment: CrossAxisAlignment.stretch,
-    //         children: [
-    //           Text(
-    //             'BROWSE CATEGORY',
-    //             style: TextStyle(
-    //                 fontSize: 22.sp,
-    //                 fontWeight: FontWeight.bold,
-    //                 color: Colors.white),
-    //           ),
-    //           SizedBox(height: 12.h),
-    //           Expanded(
-    //             child: GridView.builder(
-    //               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-    //                   crossAxisSpacing: 38.w,
-    //                   mainAxisSpacing: 38.h,
-    //                   crossAxisCount: 2),
-    //               itemCount: BrowseScreenCubit.get(context).genresList.length,
-    //               itemBuilder: (context, index) {
-    //                 return InkWell(
-    //                   onTap: () {
-    //                     showModalBottomSheet(
-    //                       isScrollControlled: true,
-    //                       backgroundColor: backgroundColor,
-    //                       context: context,
-    //                       builder: (context) {
-    //                         return MoviesByCategory(
-    //                             BrowseScreenCubit.get(context).genresList[index].name ?? '');
-    //                       },
-    //                     );
-    //                   },
-    //                   child: Container(
-    //                     decoration: BoxDecoration(
-    //                       border: Border.all(color: Color(0xFF707070)),
-    //                       borderRadius: BorderRadius.circular(15.r),
-    //                     ),
-    //                     child: Center(
-    //                       child: Text(
-    //                         BrowseScreenCubit.get(context).genresList[index].name ?? '',
-    //                         style:
-    //                         TextStyle(fontSize: 14.sp, color: Colors.white),
-    //                       ),
-    //                     ),
-    //                   ),
-    //                 );
-    //               },
-    //             ),
-    //           )
-    //         ],
-    //       ),
-    //     );
-    //   },
-    // ),
-    // );
+
 
 
 
--- lib/screens/movie_detail_screen/movie_detail_screen.dart
+++ lib/screens/movie_detail_screen/movie_detail_screen.dart
@@ -50,7 +50,7 @@ class MovieDetailScreen extends StatelessWidget {
                     CachedNetworkImage(
                       width: double.infinity,
                       height: 217.h,
-                      imageUrl: '${IMAGE_BASE_URL}${snapshot.data?.backdropPath}',
+                      imageUrl: '${IMAGE_BASE_URL}${snapshot.data?.backdropPath}' ?? '',
                       // ${arg.imageBackground}",
                       progressIndicatorBuilder:
                           (context, url, downloadProgress) => Center(
@@ -69,78 +69,87 @@ class MovieDetailScreen extends StatelessWidget {
                   ],
                 ),
                 SizedBox(height: 12.h),
-                Text(
-                  snapshot.data?.title ?? '',
-                  // arg.title ?? '',
-                  style: TextStyle(color: Colors.white, fontSize: 18.sp),
-                ),
-                SizedBox(height: 6.h),
-                Text(
-                  'Release Date: ${snapshot.data?.releaseDate}',
-                  // ${arg.releaseDate}',
-                  style: TextStyle(color: Color(0xFFB5B4B4), fontSize: 10.sp),
-                ),
-                SizedBox(height: 19.h),
-                SizedBox(
-                  height: 199.h,
-                  width: double.infinity,
-                  child: Row(
-                    children: [
-                      CachedNetworkImage(
-                        imageUrl:
-                            "${IMAGE_BASE_URL}${snapshot.data?.posterPath}",
-                        // ${arg.poster}",
-                        progressIndicatorBuilder:
-                            (context, url, downloadProgress) => Center(
+                snapshot.data?.releaseDate == null || snapshot.data?.voteAverage == null ? Center(child: CircularProgressIndicator(),) :
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Text(
+                      snapshot.data?.title ?? '',
+                      // arg.title ?? '',
+                      style: TextStyle(color: Colors.white, fontSize: 18.sp),
+                    ),
+                    SizedBox(height: 6.h),
+                    Text(
+                      'Release Date: ${snapshot.data?.releaseDate}' ?? '',
+                      // ${arg.releaseDate}',
+                      style: TextStyle(color: Color(0xFFB5B4B4), fontSize: 10.sp),
+                    ),
+                    SizedBox(height: 19.h),
+                    SizedBox(
+                      height: 199.h,
+                      width: double.infinity,
+                      child: Row(
+                        children: [
+                          CachedNetworkImage(
+                            imageUrl:
+                            "${IMAGE_BASE_URL}${snapshot.data?.posterPath}" ?? '',
+                            // ${arg.poster}",
+                            progressIndicatorBuilder:
+                                (context, url, downloadProgress) => Center(
                                 child: CircularProgressIndicator(
                                     value: downloadProgress.progress)),
-                        errorWidget: (context, url, error) => Icon(Icons.error),
-                      ),
-                      SizedBox(width: 11.w),
-                      Expanded(
-                        child: Column(
-                          children: [
-                            Expanded(
-                              child: GridView.builder(
-                                gridDelegate:
+                            errorWidget: (context, url, error) => Icon(Icons.error),
+                          ),
+                          SizedBox(width: 11.w),
+                          Expanded(
+                            child: Column(
+                              children: [
+                                Expanded(
+                                  child: GridView.builder(
+                                    gridDelegate:
                                     SliverGridDelegateWithFixedCrossAxisCount(
                                         crossAxisSpacing: 5.w,
                                         mainAxisSpacing: 3.h,
                                         crossAxisCount: 4),
-                                itemBuilder: (context, index) {
-                                  return Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(10.r),
-                                      border: Border.all(
-                                        color: Color(0xFFCBCBCB),
-                                      ),
-                                    ),
-                                    child: FittedBox(
-                                        fit: BoxFit.scaleDown,
-                                        child: Text(category[index].name ?? '', style: TextStyle(color: Color(0xFFCBCBCB), fontSize: 10.sp),)),
-                                  );
-                                },
-                                itemCount: category.length,
-                              ),
-                            ),
-                            Expanded(
-                              child: SingleChildScrollView(
-                                child: Text(
-                                  snapshot.data?.overview ?? '',
-                                  style: TextStyle(
-                                      color: Color(0xFFCBCBCB), fontSize: 13.sp),
-                                  softWrap: true,
+                                    itemBuilder: (context, index) {
+                                      return Container(
+                                        decoration: BoxDecoration(
+                                          borderRadius: BorderRadius.circular(10.r),
+                                          border: Border.all(
+                                            color: Color(0xFFCBCBCB),
+                                          ),
+                                        ),
+                                        child: FittedBox(
+                                            fit: BoxFit.scaleDown,
+                                            child: Text(category[index].name ?? '', style: TextStyle(color: Color(0xFFCBCBCB), fontSize: 10.sp),)),
+                                      );
+                                    },
+                                    itemCount: category.length,
+                                  ),
                                 ),
-                              ),
+                                Expanded(
+                                  child: SingleChildScrollView(
+                                    child: Text(
+                                      snapshot.data?.overview ?? '',
+                                      style: TextStyle(
+                                          color: Color(0xFFCBCBCB), fontSize: 13.sp),
+                                      softWrap: true,
+                                    ),
+                                  ),
+                                ),
+                                SizedBox(height: 20.h),
+                                VoteWidget('${snapshot.data?.voteAverage}' ?? '')
+                              ],
                             ),
-                            SizedBox(height: 20.h),
-                            VoteWidget('${snapshot.data?.voteAverage}')
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
+
+
+
                 SizedBox(height: 18.h),
                 Expanded(child: SimilarMoviesWidget(arg.movieId)),
 
