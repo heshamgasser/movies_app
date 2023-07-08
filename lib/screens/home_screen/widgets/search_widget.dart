6b0470ecdda762d7fb12dae7231adef7bbcf417b
Hesham Gasser <heshamgasser32@gmail.com>
2023-07-08 20:24:40 +0300

    search Screen

--- lib/screens/home_screen/widgets/search_widget.dart
+++ lib/screens/home_screen/widgets/search_widget.dart
@@ -74,98 +74,98 @@ class SearchPage extends SearchDelegate {
         }
         return query.isEmpty
             ? Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Icon(
-                    Icons.local_movies,
-                    size: 100.w,
-                    color: Color(0xFFB5B4B4),
-                  ),
-                  SizedBox(height: 10.h),
-                  Center(
-                    child: Text(
-                      'No Movies Found',
-                      style: TextStyle(color: Color(0xFFB5B4B4), fontSize: 13.sp),
-                    ),
-                  )
-                ],
-              )
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Icon(
+              Icons.local_movies,
+              size: 100.w,
+              color: Color(0xFFB5B4B4),
+            ),
+            SizedBox(height: 10.h),
+            Center(
+              child: Text(
+                'No Movies Found',
+                style: TextStyle(color: Color(0xFFB5B4B4), fontSize: 13.sp),
+              ),
+            )
+          ],
+        )
             : Expanded(
-                child: ListView.separated(
-                    itemBuilder: (context, index) {
-                      return InkWell(
-                        onTap: () {
-                          Navigator.pushReplacementNamed(
-                            context,
-                            MovieDetailScreen.routeName,
-                            arguments: ArgumentModel(
-                                movieId: snapshot.data!.results![index].id!),
-                          );
-                        },
-                        child: Container(
+          child: ListView.separated(
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () {
+                    Navigator.pushReplacementNamed(
+                      context,
+                      MovieDetailScreen.routeName,
+                      arguments: ArgumentModel(
+                          movieId: snapshot.data!.results![index].id!),
+                    );
+                  },
+                  child: Container(
+                    height: 90.h,
+                    child: Row(
+                      children: [
+                        Container(
                           height: 90.h,
-                          child: Row(
+                          width: 140.w,
+                          child: CachedNetworkImage(
+                            fit: BoxFit.fitHeight,
+                            imageUrl:
+                            '${IMAGE_BASE_URL}${snapshot.data?.results?[index].posterPath}' ??
+                                '',
+                            progressIndicatorBuilder:
+                                (context, url, downloadProgress) =>
+                                Center(
+                                  child: CircularProgressIndicator(
+                                      value: downloadProgress.progress),
+                                ),
+                            errorWidget: (context, url, error) =>
+                                Icon(Icons.error),
+                          ),
+                        ),
+                        SizedBox(width: 10.w),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment:
+                            CrossAxisAlignment.stretch,
                             children: [
-                              Container(
-                                height: 90.h,
-                                width: 140.w,
-                                child: CachedNetworkImage(
-                                  fit: BoxFit.cover,
-                                  imageUrl:
-                                      '${IMAGE_BASE_URL}${snapshot.data?.results?[index].posterPath}' ??
-                                          '',
-                                  progressIndicatorBuilder:
-                                      (context, url, downloadProgress) =>
-                                          Center(
-                                    child: CircularProgressIndicator(
-                                        value: downloadProgress.progress),
-                                  ),
-                                  errorWidget: (context, url, error) =>
-                                      Icon(Icons.error),
-                                ),
+                              Text(
+                                snapshot.data?.results?[index].title ??
+                                    '',
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    fontSize: 15.sp,
+                                    fontWeight: FontWeight.w400,
+                                    color: Colors.black),
                               ),
-                              SizedBox(width: 10.w),
-                              Expanded(
-                                child: Column(
-                                  crossAxisAlignment:
-                                      CrossAxisAlignment.stretch,
-                                  children: [
-                                    Text(
-                                      snapshot.data?.results?[index].title ??
-                                          '',
-                                      maxLines: 2,
-                                      overflow: TextOverflow.ellipsis,
-                                      style: TextStyle(
-                                          fontSize: 15.sp,
-                                          fontWeight: FontWeight.w400,
-                                          color: Colors.white),
-                                    ),
-                                    SizedBox(height: 5.h),
-                                    Text(
-                                      snapshot.data?.results?[index]
-                                              .releaseDate ??
-                                          '',
-                                      style: TextStyle(
-                                          fontSize: 13.sp, color: Colors.grey),
-                                    ),
-                                    SizedBox(height: 5.h),
-                                    VoteWidget(
-                                        '${snapshot.data?.results?[index].voteAverage}' ??
-                                            ''),
-                                  ],
-                                ),
+                              SizedBox(height: 5.h),
+                              Text(
+                                snapshot.data?.results?[index]
+                                    .releaseDate ??
+                                    '',
+                                style: TextStyle(
+                                    fontSize: 13.sp, color: Colors.grey),
                               ),
+                              SizedBox(height: 5.h),
+                              VoteWidget(
+                                  '${snapshot.data?.results?[index].voteAverage}' ??
+                                      ''),
                             ],
                           ),
                         ),
-                      );
-                    },
-                    separatorBuilder: (context, index) {
-                      return SizedBox(height: 10.h);
-                    },
-                    itemCount: snapshot.data!.results!.length),
-              );
+                      ],
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) {
+                return SizedBox(height: 10.h);
+              },
+              itemCount: snapshot.data!.results!.length),
+        );
       },
     );
   }
@@ -247,7 +247,7 @@ class SearchPage extends SearchDelegate {
                                 height: 90.h,
                                 width: 140.w,
                                 child: CachedNetworkImage(
-                                  fit: BoxFit.cover,
+                                  fit: BoxFit.fitHeight,
                                   imageUrl:
                                       '${IMAGE_BASE_URL}${snapshot.data?.results?[index].posterPath}' ??
                                           '',
@@ -275,7 +275,7 @@ class SearchPage extends SearchDelegate {
                                       style: TextStyle(
                                           fontSize: 15.sp,
                                           fontWeight: FontWeight.w400,
-                                          color: Colors.white),
+                                          color: Colors.black),
                                     ),
                                     SizedBox(height: 5.h),
                                     Text(
